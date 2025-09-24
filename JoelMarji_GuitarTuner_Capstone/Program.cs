48df580568a5f5b7b5355a01b1d1554fbd8b361a
Joel Marji <marjij1@sunypoly.edu>
2025-09-24 18:07:51 -0400

    SDL & MathNet library
    -Installed SDL2 library, basic code to ensure functionality
    -Installed MathNet.Numerics

--- JoelMarji_GuitarTuner_Capstone/Program.cs
+++ JoelMarji_GuitarTuner_Capstone/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using SDL2;
+
+class Program
+{
+    static void Main()
+    {
+        if (SDL.SDL_Init(SDL.SDL_INIT_AUDIO) < 0)
+        {
+            Console.WriteLine($"SDL_Init Error: {SDL.SDL_GetError()}");
+            return;
+        }
+        Console.WriteLine("SDL2 initialized successfully!");
+        SDL.SDL_Quit();
+    }
+}
